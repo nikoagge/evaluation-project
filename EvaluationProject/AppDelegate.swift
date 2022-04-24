2a94bcca5c6e0f00776c96be433c35fbb7d62af4
nikoagge <nikoagge@gmail.com>
2022-04-24 09:51:31 +0300

    Removed Main storyboard
    Instantiated app wIth HomeViewController, no storyboards.

--- EvaluationProject/AppDelegate.swift
+++ EvaluationProject/AppDelegate.swift
@@ -13,6 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: homeViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
     
--- EvaluationProject/Pages/Home/HomeViewController.swift
+++ EvaluationProject/Pages/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  EvaluationProject
 //
 //  Created by Nikos Aggelidis on 22/4/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()        
     }
